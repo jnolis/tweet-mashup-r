90657e0a2d06a41368b8fbde1c444b5516516e2d
Jacqueline Nolis <jnolis@users.noreply.github.com>
2020-01-09 00:37:44 -0800

    Got it working!!!! woo!!!

---
+++ app/app.R
@@ -0,0 +1,359 @@
+library(shiny)
+library(shinyjs) # for the cookie storing stuff
+library(uuid)
+library(jsonlite)
+library(httr)
+library(rtweet)
+library(future)
+
+source("twitter.R")
+
+keys <- config::get()
+
+# cache ---------------------------------------------
+
+credential_cache <- memoryCache(max_size = 16 * 1024^2, missing = NULL)
+
+user_tweet_info_cache <-
+  memoryCache(max_size = 16 * 1024^2, max_age = 86400, missing = NULL)
+
+get_user_tweet_info_cache <- function(username, token){
+  username <- fix_username(username)
+  
+  user_tweet_info <- user_tweet_info_cache$get(username)
+  if(is.null(user_tweet_info)){
+    user_tweet_info <- get_user_tweet_info(username, token)
+    user_tweet_info_cache$set(username, user_tweet_info)
+  }
+  
+  user_tweet_info
+}
+
+# helper functions ----------------------------
+
+
+# this is a modification of Michael's code to make the signature for authenticating twitter
+# API requests. It's pulled from tokens.R in the rtweet package
+oauth_sig <- function(url, method,
+                      token = NULL,
+                      token_secret = NULL,
+                      private_key = NULL, ...) {
+  httr::oauth_header(httr::oauth_signature(url, method, app, token,
+                                           token_secret, private_key, other_params = list(...)))
+}
+
+# This function creates a URL for users to click to authenticate.
+# You should use it to show a URL when users haven't authenticated yet.
+# the callback_url HAS to be in the app configuration on the developer portal,
+# and it needs to have the right http/https protocol.
+# for testing in RSTudio I found it best to user 127.0.0.1 and have shiny use port 80
+get_authorization_url <- function(app, callback_url, permission=NULL){
+  private_key <- NULL
+  response <- httr::POST("https://api.twitter.com/oauth/request_token", 
+                         oauth_sig("https://api.twitter.com/oauth/request_token",
+                                   "POST", private_key = NULL, oauth_callback = callback_url))
+  httr::stop_for_status(response)
+  params <- httr::content(response, type = "application/x-www-form-urlencoded")
+  authorize_url <- httr::modify_url("https://api.twitter.com/oauth/authenticate",
+                                    query = list(oauth_token = params$oauth_token, permission = permission))
+  authorize_url
+  
+}
+
+# Once a user authenticates them, Twitter will pass them back to the callback
+# url in the authentication one, with the results of the authentication in the query
+# of the callback url. This function takes the information from the query
+# and does the final conversion to get it into the useful format
+get_access_token <- function(app, oauth_token, oauth_verifier){
+  url <- paste0("https://api.twitter.com/oauth/access_token?oauth_token=",
+                oauth_token,"&oauth_verifier=",oauth_verifier)
+  response <- httr::POST(url, 
+                         oauth_sig(url,
+                                   "POST",
+                                   private_key = NULL))
+  if(response$status_code == 200L){
+    results <- content(response,type="text", encoding="UTF-8")
+    results <- strsplit(results,"&")[[1]]
+    results <- lapply(results, function(x) strsplit(x,"=")[[1]])
+    results <- lapply(results, function(x) setNames(list(x[2]),x[1]))
+    results <- do.call(c, results)
+    
+    results[["screen_name"]] <- NULL # since storing that might be creepy
+    results[["user_id"]] <- NULL     # since storing that might be creepy
+    
+    results
+  } else {
+    NULL
+  }
+}
+
+# this code is to handle the fact that Shiny (frustratingly) does not have built in cookie
+# manipulation functions. With this code from Calli Gross (https://calligross.de/post/using-cookie-based-authentication-with-shiny/)
+# it's now simple to do so!
+# Notice that the "expires: 90" means the cookies expire after 90 days.
+
+jsCode <- '
+  shinyjs.getcookie = function(params) {
+    var cookie = Cookies.get("id");
+    if (typeof cookie !== "undefined") {
+      Shiny.onInputChange("jscookie", cookie);
+    } else {
+      var cookie = "";
+      Shiny.onInputChange("jscookie", cookie);
+    }
+  }
+  shinyjs.setcookie = function(params) {
+    Cookies.set("id", escape(params), { expires: 90 });  
+    Shiny.onInputChange("jscookie", params);
+  }
+  shinyjs.rmcookie = function(params) {
+    Cookies.remove("id");
+    Shiny.onInputChange("jscookie", "");
+  }
+'
+# The app we'll be using. I didn't give it a name since it doesn't seem to matter
+app <- oauth_app(
+  app = "",
+  key = keys$consumer_key,
+  secret = keys$consumer_secret
+)
+
+
+ui <- div(
+  tags$head(
+    tags$title("Tweet Mashup!"),
+    tags$link(rel="shortcut icon", href="favicon.ico", type="image/x-icon"),
+    tags$meta(name="viewport", content="width=device-width, initial-scale=1, maximum-scale=1"),
+    tags$meta(`http-equiv`="x-ua-compatible", content="ie=edge"),
+    tags$meta(name="description", content = "Generate some pet names!"),
+    tags$script(src = "js/js.cookie.min.js"),
+    tags$link(rel = "stylesheet",
+              type = "text/css",
+              href = "site.css"),
+    tags$link(rel="stylesheet",
+              href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u",
+              crossorigin="anonymous"),
+    tags$script(src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
+                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa",
+                crossorigin="anonymous")
+    
+  ),
+  useShinyjs(),
+  extendShinyjs(text = jsCode),
+  
+  div(class="navbar navbar-static-top",role="navigation",
+      div(class="container",
+          tags$ul(class = "nav navbar-nav navbar-right",
+                  tags$li(
+                    p(class="navbar-text navbar-right",
+                      HTML("Made by <a href=\"https://nolisllc.com\" target=\"_blank\">Nolis, LLC</a> with help from <a href=\"http://jesseddy.com\" class=\"navbar-link\" target=\"_blank\">Jess Eddy</a>")
+                    )
+                  )
+          )
+      )
+  ),
+  
+  tags$section(class="bg-primary",
+               div(class="container text-center",
+                   h1("Tweet mashup!"),
+                   h3("Combine tweets from two Twitter accounts for one awesome tweet!"))),
+  
+  div(class="container",
+      uiOutput("try_it", class="try-it-input"), # this will either show a link to authenticate or some tweets
+      uiOutput("generated_tweet")
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  # store the user id that will span sessions. It's a UUID stored in the user cookies
+  user_id <- reactive({
+    js$getcookie()
+    
+    # If the cookie loading but it's empty that means we have a new user 
+    # and need to give them an id
+    if (!is.null(input$jscookie) && 
+        is.character(input$jscookie) &&
+        nchar(trimws(input$jscookie)) == 0) {
+      user_id <- paste0(sample(c(letters,0:9), 32, replace=TRUE),collapse="")
+      js$setcookie(user_id)
+    } else {
+      user_id <- input$jscookie
+    }
+    user_id
+  })
+  
+  # this will be NULL if the user hasn't logged in yet, otherwise 
+  # it will have the valid twitter token for user
+  access_token <- reactive({
+    if(is.null(user_id())){ # if we don't even have a UUID yet then there are no keys
+      access_token <- NULL
+    } else {
+      
+      # check if we have saved the keys in the cache
+      access_token <- credential_cache$get(user_id())
+      
+      if(is.null(access_token)){
+        # is the user is coming in from having just authenticated? 
+        # if yes save the tokens, if not then no keys to user
+        query <- getQueryString(session)
+        if(!is.null(query) 
+           && !is.null(query$oauth_token) 
+           && !is.null(query$oauth_verifier)){ 
+          access_token <- get_access_token(app, query$oauth_token, query$oauth_verifier)
+          if(!is.null(access_token)){
+            credential_cache$set(user_id(), access_token)
+          }
+        }
+      }
+    }
+    # turn the information from the file into a valid token object
+    if(!is.null(access_token)){
+      create_token(app="", 
+                   keys$consumer_key, 
+                   keys$consumer_secret, 
+                   access_token = access_token$oauth_token, 
+                   access_secret = access_token$oauth_token_secret)
+    }
+  })
+  
+  username_1 <- reactive({
+    original <- input$username_1
+    username <- NULL
+    if(!is.null(original)){
+      fixed_username <- fix_username(original)
+      if(nchar(fixed_username) > 0){
+        username <- fixed_username
+      }
+    }
+    username
+  })
+  
+  username_2 <- reactive({
+    original <- input$username_2
+    username <- NULL
+    if(!is.null(original)){
+      fixed_username <- fix_username(original)
+      if(nchar(fixed_username) > 0){
+        username <- fixed_username
+      }
+    }
+    username
+  })
+  
+  user_tweet_info_1 <- reactive({
+    input$generate
+    username <- isolate({username_1()})
+    access_token <- isolate({access_token()})
+    if(!is.null(username)){
+      get_user_tweet_info_cache(username, access_token)
+    } else {
+      NULL
+    }
+  })
+  
+  user_tweet_info_2 <- reactive({
+    input$generate
+    username <- isolate({username_2()})
+    access_token <- isolate({access_token()})
+    if(!is.null(username)){
+      get_user_tweet_info_cache(username, access_token)
+    } else {
+      NULL
+    }
+  })
+  
+  generated_tweet <- reactive({
+    input$generate
+    user_tweet_info_1 <- isolate({user_tweet_info_1()})
+    user_tweet_info_2 <- isolate({user_tweet_info_2()})
+    if(!is.null(user_tweet_info_1) && !is.null(user_tweet_info_2)){
+      make_combined_tweet(user_tweet_info_1, user_tweet_info_2)
+    } else {
+      NULL
+    }
+  })
+  
+  output$generated_tweet <- renderUI({
+    
+    generated_tweet <- generated_tweet()
+    
+    user_tweet_info_1 <- isolate({user_tweet_info_1()})
+    user_tweet_info_2 <- isolate({user_tweet_info_2()})
+    if(!is.null(generated_tweet)){
+      user_info_1 <- user_tweet_info_1$user_info
+      user_info_2 <- user_tweet_info_2$user_info
+      div(class="output-ui",
+          div(class="row",
+              div(class="col-md-4 col-lg-4 left-name hidden-sm hidden-xs",
+                  h4(user_info_1$name),
+                  h6(tags$em(user_info_1$screen_name))
+              ),
+              div(class="overlapping-images col-md-4 col-lg-4",
+                  img(src=user_info_1$image_url, class="img-circle img-left", width = "128", height ="128"),
+                  img(src=user_info_2$image_url, class="img-circle img-right", width = "128", height ="128")
+              ),
+              div(class="col-md-4 col-lg-4 right-name hidden-sm hidden-xs",
+                  h4(user_info_2$name),
+                  h6(tags$em(user_info_2$screen_name))
+              )
+          ),
+          div(class="row text-center",
+              HTML("TweetMashup.com by <a href=\"https://nolisllc.com\" target=\"_blank\">Nolis, LLC</a>")
+          ),
+          div(class="row",
+              p(class="tweet-text text-center", generated_tweet)
+          )
+      )
+    } else {
+      div()
+    }
+  })
+  
+  # either show the authentication URL or a few tweets
+  output$try_it <- renderUI({
+    if(is.null(access_token())){
+      url <- get_authorization_url(app, callback_url = "http://127.0.0.1")
+      tags$form(
+        div(class="form-group row",
+            div(class="col-xs-4",
+                div(class="input-group",
+                    span(class="input-group-addon","@"),
+                    tags$input(type="text",class="form-control",  id="username_1", disabled=NA)
+                )),
+            div(class="col-xs-1",h1("&", class="ampersand text-center")),
+            div(class="col-xs-4",
+                div(class="input-group",
+                    span(class="input-group-addon","@"),
+                    tags$input(type="text",class="form-control", id="username_2", disabled=NA)
+                )),
+            div(class="col-xs-3",a(class="btn btn-primary", href = url, "Authorize to make your own!")))
+      )
+    } else {
+      tags$form(
+        div(class="form-group row",
+            div(class="col-xs-4",
+                div(class="input-group",
+                    span(class="input-group-addon","@"),
+                    tags$input(type="text",class="form-control",  id="username_1")
+                )),
+            div(class="col-xs-1",h1("&", class="ampersand text-center")),
+            div(class="col-xs-4",
+                div(class="input-group",
+                    span(class="input-group-addon","@"),
+                    tags$input(type="text",class="form-control", id="username_2")
+                )),
+            div(class="col-xs-3",actionButton("generate","Go!", class="btn btn-primary")))
+      )
+    }
+    
+  })
+  
+}
+
+# make sure you use port 80 or whatever you put in the twitter developer portal
+# in RStudio if you hit "Run App" it ignore the port, so watch out!
+shinyApp(ui = ui, server = server, options=list(port=80L, launch.browser=FALSE))
+
---
+++ app/twitter.R
@@ -0,0 +1,123 @@
+library(rtweet)
+library(httr)
+library(purrr)
+library(dplyr)
+
+max_tweet_length <- 280
+
+fix_username <- function(username){
+  gsub("[^[:alnum:]_]", "", username)
+}
+
+get_user_facts <- function(username,token){
+  large_image_urls <- function(urls){
+    sub("_normal\\.", "_400x400.", urls)
+  }
+  fix_https <- function(urls){
+    sub("^http://","https://", urls)
+  }
+  
+  user <- lookup_users(username,token=token)
+  image_url <-  large_image_urls(user$profile_image_url)
+  list(name = user$name, 
+       screen_name = user$screen_name,
+       image_url = image_url
+       )
+}
+
+get_tweets <- function(username, token, n = 640){
+  raw_tweets <- get_timeline(username,n=n, token=token)
+  if(nrow(raw_tweets) > 0){
+    filtered <- raw_tweets[!raw_tweets$is_retweet & is.na(raw_tweets$reply_to_status_id),c("status_id","text")]
+    tweets <- setNames(filtered$text,filtered$status_id)
+    
+    clean_tweets <- function(tweets){
+      tweets <- gsub("\\s+", " ", tweets)
+      tweets <- gsub("https?://.*\\b","",tweets)
+      tweets <- trimws(tweets)
+      tweets
+    }
+    
+    tweets <- clean_tweets(tweets)
+  } else {
+    tweets <- list()
+  }
+  tweets
+}
+
+make_word_lookup <- function(tweets){
+  process_one_word_lookup <- function(word_list, tweet_id){
+    word_char_num <- nchar(word_list)
+    word_cum_sum <- cumsum(word_char_num) + 0:(length(word_char_num)-1)
+    
+    characters_before <- word_cum_sum - word_char_num
+    characters_after <- sum(word_char_num) - word_cum_sum 
+    
+    one_word_lookup <- transpose(list(
+      word = word_list,
+      clean_word = clean_words(word_list),
+      chars_before_word = characters_before,
+      chars_in_word = word_char_num,
+      chars_after_word = characters_after,
+      tweet_id = rep(tweet_id, length(word_list))))
+    
+    one_word_lookup <- one_word_lookup[c(-1,-length(one_word_lookup))] 
+    # remove the first and last words because they're dull
+    
+  }
+  
+  clean_words <- function(words){
+    tolower(gsub("[^[:alpha:]]","",words))
+  }
+  
+  word_lists <- strsplit(tweets," ")
+  
+  word_lookup <- flatten_dfr(imap(word_lists, process_one_word_lookup))
+  word_lookup
+}
+
+get_user_tweet_info <- function(username, token){
+  username <- fix_username(username)
+  user_info <- get_user_facts(username, token)
+  tweets <- get_tweets(username, token)
+  word_lookup <- make_word_lookup(tweets)
+  list(username = username,
+       user_info = user_info,
+       tweets = tweets,
+       word_lookup = word_lookup)
+}
+
+make_combined_tweet <- function(user_tweet_info_1, user_tweet_info_2){
+  tweets_1 <- user_tweet_info_1$tweets
+  tweets_2 <- user_tweet_info_2$tweets
+  word_lookup_1 <- user_tweet_info_1$word_lookup
+  word_lookup_2 <- user_tweet_info_2$word_lookup
+  
+  all_splits <- inner_join(rename_at(word_lookup_1,vars(-clean_word), function(x) paste0(x,"_1")),
+                      rename_at(word_lookup_2,vars(-clean_word), function(x) paste0(x,"_2")),
+                      by="clean_word")
+  all_splits <- 
+    mutate(all_splits,
+           fst_1_valid = chars_before_word_1 + chars_in_word_1 + chars_after_word_2 <= max_tweet_length,
+           fst_2_valid = chars_before_word_2 + chars_in_word_2 + chars_after_word_1 <= max_tweet_length)
+  split <- sample_n(all_splits, 1, weight = fst_1_valid + fst_2_valid)
+  
+  if(split$fst_1_valid & split$fst_2_valid){
+    fst_1 <- sample(c(F,T),1)
+  } else if(split$fst_1_valid){
+    fst_1 <- TRUE
+  } else {
+    fst_1 <- FALSE
+  }
+  
+  if(fst_1){
+    fst_half <- substr(tweets_1[[split$tweet_id_1]], 1, with(split,chars_before_word_1 + chars_in_word_1))
+    snd_half <- substr(tweets_2[[split$tweet_id_2]], with(split, chars_before_word_2 + chars_in_word_2 + 1), max_tweet_length)
+  } else {
+    fst_half <- substr(tweets_2[[split$tweet_id_2]], 1, with(split,chars_before_word_2 + chars_in_word_2))
+    snd_half <- substr(tweets_1[[split$tweet_id_1]], with(split, chars_before_word_1 + chars_in_word_1 + 1), max_tweet_length)
+  }
+  
+  generated_tweet <- paste0(fst_half, snd_half)
+  generated_tweet
+}
